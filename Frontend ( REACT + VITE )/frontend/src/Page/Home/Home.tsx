2c8556175116b80986fafd115ba720fe42d9c7d0
Iagovila17 <iagovila17@gmail.com>
2025-01-16 21:27:22 +0100

    Se crea la Pagina de Home con la Frase de PAGIN EN CONTRUCCION y se corrigen errores de packge-lock.json y del tsconfig

---
+++ Frontend ( REACT + VITE )/frontend/src/Page/Home/Home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home: React.FC = () => {
+    return(
+        <div> 
+            <h1>La Pagina Esta en Desarollo</h1>
+            <p>Disculpe las Molestias</p>
+        </div>
+    )
+}
+
+export default Home;
