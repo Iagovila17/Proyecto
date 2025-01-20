901942b00eb152b8ce913ff85f326e09beb396e7
iagovila17 <iagovila17@gmail.com>
2025-01-20 20:56:55 +0100

    se añade una imagen para la pagina web donde se ve que esta en contruccion y se mete en asset en una carpeta provisional. Se modifica el el vite.config.ts para que pueda conectarse cualquiera a la pagina cuando el servidor este activo

--- Frontend ( REACT + VITE )/frontend/src/Page/Home/Home.tsx
+++ Frontend ( REACT + VITE )/frontend/src/Page/Home/Home.tsx
@@ -1,8 +1,10 @@
 import React from "react";
+import "./Home.css";
 
 const Home: React.FC = () => {
     return(
         <div> 
+            <img src="src/assets/IMAGE/robot.png" alt="Robot" />
             <h1>La Pagina Esta en Desarollo</h1>
             <p>Disculpe las Molestias</p>
         </div>
--- Frontend ( REACT + VITE )/frontend/vite.config.ts
+++ Frontend ( REACT + VITE )/frontend/vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Permite conexiones desde cualquier IP
+    port: 5174, // Define el puerto del servidor (opcional, 5174 por defecto)
+  },
 })
